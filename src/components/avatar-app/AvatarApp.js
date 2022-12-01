0319d1a7f772410a5a00f0c9620a32a9e4e92988
Daria Movchan <dm@dvizh.io>
2022-12-02 00:17:31 +0200

    add styles

--- src/components/avatar-app/AvatarApp.js
+++ src/components/avatar-app/AvatarApp.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Button from '../button/Button';
+import './AvatarApp.css'
 
 export default class AvatarApp extends React.Component {
+    constructor() {
+        super()
+        let newAvatars = [];
+        let avatarsUsed = 0;
+    }
 
+    // async function getAvatars() {
+    //     let response = await axios.get('https://tinyfac.es/api/data?limit=50&quality=0')
+    //     return response.data
+    //   }
 
     render() {
-       return <Button buttonText='button'></Button>
+        return <Button buttonText='button'></Button>
     }
 }
--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Button.css'
 
 export default class Button extends React.Component {
 
 
     render() {
-       return <button onClick={this.props.onClickFunction} type="button">{this.props.buttonText}</button>
+       return <button className='button' onClick={this.props.onClickFunction} type="button">{this.props.buttonText}</button>
     }
 }
